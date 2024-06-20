39db6ddfb20597c1c4990340239ab364e52c468d
Cosmin Popa <cosmin7popa@gmail.com>
2024-06-21 00:18:39 +0300

    Add keyboard interface
    Signed-off-by: Cosmin Popa <cosmin7popa@gmail.com>

--- keyboard.c
+++ keyboard.c
@@ -1,11 +1,36 @@
 #include "keyboard.h"
 #include "idt.h"
+#include "utils.h"
 
 #define KBD_IRQ 1
+#define KBD_PORT 0x60
+
+// One-element queue.
+static Key current_key_pressed = Key_None;
+static Key current_key_released = Key_None;
 
 static void keyboard_irq_handler(void)
 {
-    
+    uint8_t key = inb(KBD_PORT);
+
+    if (key == 0x11)
+        current_key_pressed = Key_W;
+    if (key == 0x91)
+        current_key_released = Key_W;
+}
+
+Key keyboard_pop_key_pressed(void)
+{
+    Key key = current_key_pressed;
+    current_key_pressed = Key_None;
+    return key;
+}
+
+Key keyboard_pop_key_released(void)
+{
+    Key key = current_key_released;
+    current_key_released = Key_None;
+    return key;
 }
 
 void keyboard_init(void)
--- keyboard.h
+++ keyboard.h
@@ -1,6 +1,13 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
+typedef enum Key {
+    Key_None,
+    Key_W,
+} Key;
+
 void keyboard_init(void);
+Key keyboard_pop_key_pressed(void);
+Key keyboard_pop_key_released(void);
 
 #endif // KEYBOARD_H
