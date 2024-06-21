bc1ddda5bb724b9f1af7eb913d549f84ae780c67
Cosmin Popa <cosmin7popa@gmail.com>
2024-06-21 13:20:24 +0300

    Add screen class for terminal buffering
    Signed-off-by: Cosmin Popa <cosmin7popa@gmail.com>

---
+++ screen.c
@@ -0,0 +1,34 @@
+#include "screen.h"
+
+void screen_display(Screen* screen, TermHandle* term)
+{
+    for (size_t y = 0; y < VGA_HEIGHT; y++)
+        for (size_t x = 0; x < VGA_WIDTH; x++)
+            terminal_put_entry(term, screen->pixels[y][x], x, y);
+}
+
+void screen_clear(Screen* screen, VgaColor color)
+{
+    for (size_t y = 0; y < VGA_HEIGHT; y++)
+        for (size_t x = 0; x < VGA_WIDTH; x++)
+            screen_draw_pixel(screen, color, x, y);
+}
+
+void screen_draw_pixel(Screen* screen, VgaColor color, size_t x, size_t y)
+{
+    if (x < VGA_WIDTH && y < VGA_HEIGHT)
+        screen->pixels[y][x] = vga_entry(' ', vga_entry_color(color, color));
+}
+
+void screen_put_entry(Screen* screen, uint16_t entry, size_t x, size_t y)
+{
+    if (x < VGA_WIDTH && y < VGA_HEIGHT)
+        screen->pixels[y][x] = entry;
+}
+
+void screen_write_string(Screen* screen, char const* string, size_t x, size_t y, uint8_t color_entry)
+{
+    while (*string)
+        screen_put_entry(screen, vga_entry(*string++, color_entry), x++, y);
+}
+
---
+++ screen.h
@@ -0,0 +1,22 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include "term.h"
+
+typedef struct Screen {
+    uint16_t pixels[VGA_HEIGHT][VGA_WIDTH];
+} Screen;
+
+void screen_display(Screen* screen, TermHandle* term);
+void screen_clear(Screen* screen, VgaColor color);
+void screen_draw_pixel(Screen* screen, VgaColor color, size_t x, size_t y);
+void screen_put_entry(Screen* screen, uint16_t entry, size_t x, size_t y);
+void screen_write_string(Screen* screen, char const* string, size_t x, size_t y, uint8_t color_entry);
+
+#define __screen_write_string(SCR, STR, X, Y, COLOR, ...) screen_write_string(SCR, STR, X, Y, COLOR)
+#define screen_write_string(SCR, STR, X, ...)               \
+    __screen_write_string(SCR, STR, X, __VA_ARGS__,         \
+    vga_entry_color(VgaColor_White, VgaColor_Black),        \
+    /* ISO C99: at least one arg in "..." */ 0)
+
+#endif // SCREEN_H
