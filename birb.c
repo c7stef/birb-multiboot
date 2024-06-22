6541892a94cd00f72ec1e70b7dc264abd591979a
Cosmin Popa <cosmin7popa@gmail.com>
2024-06-22 13:50:38 +0300

    Add collision checks, make unit conversion uniform
    Signed-off-by: Cosmin Popa <cosmin7popa@gmail.com>

--- birb.c
+++ birb.c
@@ -1,6 +1,11 @@
 #include "birb.h"
 
-static char const* const birb_sprite[] = {
+#include "tunnel.h"
+
+#define BIRB_HEIGHT     6
+#define BIRB_WIDTH      8
+
+static char const birb_sprite[BIRB_HEIGHT][BIRB_WIDTH] = {
     "     WW ",
     "     WWB",
     "   WWWW ",
@@ -9,10 +14,18 @@ static char const* const birb_sprite[] = {
     "  B B   "
 };
 
-static Vec2 jump_velocity = { .x = 0, .y = -14 };
+static uint8_t const collision_mask[BIRB_HEIGHT] = {
+    0b00000110,
+    0b00000111,
+    0b00011110,
+    0b00111110,
+    0b01111100,
+    0b00101000   
+};
+
+static Vec2 jump_velocity = { .x = 0, .y = -16 };
 
 #define VELOCITY_STEP 2
-#define POSITION_STEP 10
 
 void birb_initialize(Birb* birb)
 {
@@ -24,6 +37,26 @@ void birb_jump(Birb* birb)
     birb->velocity = jump_velocity;
 }
 
+bool birb_check_tunnel_collision(Birb* birb, TunnelPool* tunnels)
+{
+    Vec2 pos = vec2_decimal_to_pixel(birb->position);
+    
+    for (size_t i = 0; i < BIRB_HEIGHT; i++) {
+        uint8_t mask = collision_mask[i];
+        uint8_t bitmask = 0b10000000;
+
+        for (size_t j = 0; j < BIRB_WIDTH; j++) {
+            Vec2 pixel_pos = { pos.x + j, pos.y + i};
+            if (mask & bitmask && tunnel_check_collision_at(tunnels, pixel_pos))
+                return true;
+
+            bitmask >>= 1;
+        }
+    }
+
+    return false;
+}
+
 void birb_update(Birb* birb)
 {
     birb->position = (Vec2) {
@@ -32,17 +65,19 @@ void birb_update(Birb* birb)
     };
 
     birb->velocity.y++;
+
+    // Troll physics. Increase velocity artificially to prevent birb from
+    // staying still
     if (birb->velocity.y >= 0 && birb->velocity.y < VELOCITY_STEP)
         birb->velocity.y = VELOCITY_STEP;
 }
 
 void birb_render(Birb* birb, Screen* screen)
 {
-    size_t sprite_height = sizeof birb_sprite / sizeof *birb_sprite;
-    size_t sprite_width = strlen(*birb_sprite);
+    Vec2 pos = vec2_decimal_to_pixel(birb->position);
 
-    for (size_t i = 0; i < sprite_height; i++) {
-        for (size_t j = 0; j < sprite_width; j++) {
+    for (size_t i = 0; i < BIRB_HEIGHT; i++) {
+        for (size_t j = 0; j < BIRB_WIDTH; j++) {
             char sprite_ch = birb_sprite[i][j];
 
             if (sprite_ch == ' ')
@@ -50,9 +85,7 @@ void birb_render(Birb* birb, Screen* screen)
 
             VgaColor color = vga_color_map[(size_t)sprite_ch];
 
-            screen_draw_pixel(screen, color,
-                birb->position.x / POSITION_STEP + j,
-                birb->position.y / POSITION_STEP + i);
+            screen_draw_pixel(screen, color, pos.x + j, pos.y + i);
         }
     }
 }
--- birb.h
+++ birb.h
@@ -1,6 +1,8 @@
 #ifndef BIRB_H
 #define BIRB_H
 
+#include <stdbool.h>
+
 #include "screen.h"
 #include "utils.h"
 
@@ -10,9 +12,14 @@ typedef struct Birb
     Vec2 velocity;
 } Birb;
 
+typedef struct TunnelPool TunnelPool;
+
 void birb_initialize(Birb* birb);
 void birb_jump(Birb* birb);
+bool
+ birb_check_tunnel_collision(Birb* birb, TunnelPool* tunnels);
 void birb_update(Birb* birb);
 void birb_render(Birb* birb, Screen* screen);
+
 
 #endif // BIRB_H
