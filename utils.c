02bce7bb3570c16b2fb8258109228c1888dd5ee0
Cosmin Popa <cosmin7popa@gmail.com>
2024-06-22 13:53:57 +0300

    Add rng and unit conversion
    Signed-off-by: Cosmin Popa <cosmin7popa@gmail.com>

--- utils.c
+++ utils.c
@@ -1,8 +1,36 @@
 #include "utils.h"
 #include "screen.h"
 
+// Declarations so compiler emits symbols for inline functions
 void outb(uint16_t port, uint8_t val);
 uint8_t inb(uint16_t port);
+
+Vec2 vec2_decimal_to_pixel(Vec2 decimal)
+{
+	return (Vec2) {
+		decimal_to_pixel(decimal.x),
+		decimal_to_pixel(decimal.y)
+	};
+}
+
+Vec2 vec2_pixel_to_decimal(Vec2 pixel)
+{
+	return (Vec2) {
+		pixel_to_decimal(pixel.x),
+		pixel_to_decimal(pixel.y)
+	};
+}
+
+int decimal_to_pixel(int decimal)
+{
+	return decimal / DECIMAL_UNIT;
+}
+
+int pixel_to_decimal(int pixel)
+{
+	return pixel * DECIMAL_UNIT;
+}
+
 
 size_t strlen(const char* str)
 {
@@ -30,6 +58,19 @@ void sleep(size_t ticks)
 void micro_sleep(void)
 {
 	outb(0x80, 0);
+}
+
+static unsigned long next = 1;
+
+int rand(void)
+{
+    next = next * 1103515245 + 12345;
+    return (unsigned int) (next / 65536) % 32768;
+}
+
+void srand(unsigned int seed)
+{
+    next = seed;
 }
 
 noreturn void panic(char const* message)
--- utils.h
+++ utils.h
@@ -6,15 +6,26 @@
 
 #include "term.h"
 
+#define RAND_MAX 32767
+#define DECIMAL_UNIT 10
+
 typedef struct Vec2 {
     int x;
     int y;
 } Vec2;
 
+Vec2 vec2_decimal_to_pixel(Vec2 decimal);
+Vec2 vec2_pixel_to_decimal(Vec2 pixel);
+int decimal_to_pixel(int decimal);
+int pixel_to_decimal(int pixel);
+
 size_t strlen(const char* str);
 void strcat(char* dst, char const* src);
 void sleep(size_t ticks);
 void micro_sleep(void);
+
+int rand(void);
+void srand(unsigned int seed);
 
 noreturn void panic(char const* message);
 noreturn void hang(void);
